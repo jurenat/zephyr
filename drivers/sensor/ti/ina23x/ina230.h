458e5a134c503f097935f0c38b0ac2971c027d90
Tomáš Juřena <jurenatomas@gmail.com>
2024-12-06 08:12:42 +0100

    drivers: sensor: ti: ina23x: Refactor INA230 trigger
    Trigger use own thread or system work queue to handle interrupts. This
    allows to read the device state in the trigger handler.
    Also trigger can distinguish between data ready and alert functionality,
    and does not require prepare device state before setting trigger.
    
    Signed-off-by: Tomáš Juřena <jurenatomas@gmail.com>

--- drivers/sensor/ti/ina23x/ina230.h
+++ drivers/sensor/ti/ina23x/ina230.h
@@ -39,12 +39,20 @@ struct ina230_data {
 	int16_t current;
 	uint16_t bus_voltage;
 	uint16_t power;
+	int16_t shunt_voltage;
+	uint16_t mask;
 #ifdef CONFIG_INA230_TRIGGER
 	const struct device *gpio;
 	struct gpio_callback gpio_cb;
-	struct k_work work;
 	sensor_trigger_handler_t handler_alert;
 	const struct sensor_trigger *trig_alert;
+	sensor_trigger_handler_t handler_cnvr;
+	const struct sensor_trigger *trig_cnvr;
+#if CONFIG_INA230_TRIGGER_OWN_THREAD
+	struct k_sem sem;
+#elif CONFIG_INA230_TRIGGER_GLOBAL_THREAD
+	struct k_work work;
+#endif
 #endif /* CONFIG_INA230_TRIGGER */
 };
 
@@ -56,7 +64,6 @@ struct ina230_config {
 	uint8_t power_scale;
 	uint32_t uv_lsb;
 #ifdef CONFIG_INA230_TRIGGER
-	bool trig_enabled;
 	uint16_t mask;
 	const struct gpio_dt_spec alert_gpio;
 	uint16_t alert_limit;
